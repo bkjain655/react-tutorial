f6ba5153f6c362d5f41ac6cd8b370f0c96ed1ada
Bhavesh Kumar <bkumar@gainsight.com>
2020-08-28 15:42:27 +0530

    Updates

---
+++ precache-manifest.cb7308dc2475a70e0436dcf3fac93239.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d044653b63c94b7787c5b9e13d24eb5b",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "2bace4beccc06f6a7327",
+    "url": "/react-tutorial/static/css/main.f78c5094.chunk.css"
+  },
+  {
+    "revision": "4c78f869fc1ee9642945",
+    "url": "/react-tutorial/static/js/2.416264a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.416264a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bace4beccc06f6a7327",
+    "url": "/react-tutorial/static/js/main.a6e8a598.chunk.js"
+  },
+  {
+    "revision": "de373458209362eff551",
+    "url": "/react-tutorial/static/js/runtime-main.dffa572e.js"
+  }
+]);
